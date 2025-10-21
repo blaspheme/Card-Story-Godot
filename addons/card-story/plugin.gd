a51a621d563a9546d8ef592d3ea30c8ed3fddf08
Erudite <mooncrime93@gamil.com>
2025-10-21 10:36:08 +0900

    add: Plugin Reloader

--- addons/card-story/plugin.gd
+++ addons/card-story/plugin.gd
@@ -3,15 +3,16 @@ extends EditorPlugin
 
 # 主编辑器场景
 var main_editor_panel: Control
+const PLUGIN_NAME := "Card Story" # 替换成你自己的文件夹名
 
 func _has_main_screen() -> bool:
 	return true
 
 func _get_plugin_name() -> String:
-	return "Card Story"
+	return PLUGIN_NAME
 
 func _get_plugin_icon() -> Texture2D:
-	return load("res://addons/card-story/icons/card_icon.svg")
+	return preload("res://addons/card-story/icons/card_icon.svg")
 
 func _enter_tree():
 	main_editor_panel = preload("res://addons/card-story/card_story_editor.tscn").instantiate()
