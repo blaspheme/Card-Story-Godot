9ed5fade8644e0ed81d9eafe078e7ea7028a186f
Erudite <mooncrime93@gamil.com>
2025-10-21 09:53:06 +0900

    feat: 插件场景集成

--- addons/card-story/plugin.gd
+++ addons/card-story/plugin.gd
@@ -1,6 +1,9 @@
 @tool
 extends EditorPlugin
 
+# 主编辑器场景
+var main_editor_panel: Control
+
 func _has_main_screen() -> bool:
 	return true
 
@@ -8,10 +11,16 @@ func _get_plugin_name() -> String:
 	return "Card Story"
 
 func _get_plugin_icon() -> Texture2D:
-	return load("res://addons/card-story/icons/card_icon_16.svg")
+	return load("res://addons/card-story/icons/card_icon.svg")
 
 func _enter_tree():
-	pass
+	main_editor_panel = preload("res://addons/card-story/card_story_editor.tscn").instantiate()
+	EditorInterface.get_editor_main_screen().add_child(main_editor_panel)
+	main_editor_panel.hide()
+
 
 func _exit_tree():
-	pass
+	main_editor_panel.queue_free()
+
+func _make_visible(visible: bool) -> void:
+	main_editor_panel.visible = visible
